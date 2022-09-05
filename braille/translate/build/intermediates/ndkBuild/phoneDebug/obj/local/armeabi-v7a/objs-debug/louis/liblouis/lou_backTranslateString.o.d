d1db439b30f3b16132e8447361f6d045d15b808c
Quintin Balsdon <quintinb@spotify.com>
2022-09-05 17:55:41 +0100

    Added ignore

--- braille/translate/build/intermediates/ndkBuild/phoneDebug/obj/local/armeabi-v7a/objs-debug/louis/liblouis/lou_backTranslateString.o.d
+++
@@ -1,8 +0,0 @@
-/Users/quintinb/repo/talkback/braille/translate/build/intermediates/ndkBuild/phoneDebug/obj/local/armeabi-v7a/objs-debug/louis/liblouis/lou_backTranslateString.o: \
-  /Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/lou_backTranslateString.c \
-  /Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/internal.h \
-  /Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/liblouis.h
-
-/Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/internal.h:
-
-/Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/liblouis.h:
--- braille/translate/build/intermediates/ndkBuild/wearDebug/obj/local/x86_64/objs-debug/louis/liblouis/pattern.o.d
+++
@@ -1,8 +0,0 @@
-/Users/quintinb/repo/talkback/braille/translate/build/intermediates/ndkBuild/wearDebug/obj/local/x86_64/objs-debug/louis/liblouis/pattern.o: \
-  /Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/pattern.c \
-  /Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/internal.h \
-  /Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/liblouis.h
-
-/Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/internal.h:
-
-/Users/quintinb/repo/talkback/braille/translate/src/phone/jni/third_party/liblouis/liblouis/liblouis.h:
